a3493492eb4e5047101dd11cba43e072214836ef
Alexandre Rodriguez <alexandre@identiq.net>
2025-09-27 19:19:55 +0200

    feat(auth): init

--- apps/api/src/lib/withAuth.ts
+++ apps/api/src/lib/withAuth.ts
@@ -11,7 +11,35 @@ export function withAuth<T, U extends any[]>(
   mockSuccess: T,
 ) {
   return async function (...args: U): Promise<T> {
+    /*
+    const [req] = args
+    const authHeader =
+      req.headers.authorization ??
+      (req.headers["sec-websocket-protocol"]
+        ? `Bearer ${req.headers["sec-websocket-protocol"]}`
+        : null);
+    if (!authHeader) {
+      return { success: false, error: "Unauthorized", status: 401 } as T;
+    }
+    const token = authHeader.split(" ")[1]; // Extract the token from "Bearer <token>"
+    if (!token) {
+      return {
+        success: false,
+        error: "Unauthorized: Token missing",
+        status: 401,
+      } as T;
+    }
+
+    if (token === process.env.TEST_API_KEY) {
+      return { success: true, ...(mockSuccess || {}) } as T;
+    }
+    
+
+    return { success: false, error: "Unauthorized", status: 401 } as T;
+    */
+
     const useDbAuthentication = process.env.USE_DB_AUTHENTICATION === "true";
+
     if (!useDbAuthentication) {
       if (warningCount < 5) {
         logger.warn("You're bypassing authentication");
